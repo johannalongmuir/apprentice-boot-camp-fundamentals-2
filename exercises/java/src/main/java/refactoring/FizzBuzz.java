bf15f8c590dd38b617ab371c654268006b5bb960
Johanna Longmuir <johannalongmuir@gmail.com>
2019-12-17 11:55:47 +0000

    Minor refactor

--- exercises/java/src/main/java/refactoring/FizzBuzz.java
+++ exercises/java/src/main/java/refactoring/FizzBuzz.java
@@ -7,38 +7,41 @@ import java.nio.charset.StandardCharsets;
 class FizzBuzz {
 
     private int number;
-    private int FizzDivideThreeCheck;
-    private int BuzzDivideFiveCheck = new int[]{0, 0, 0, 0, 0}.length;
+    private int FizzDivideThreeCounter;
+    private int BuzzDivideFiveCounter = new int[]{0, 0, 0, 0, 0}.length;
 
 
-    String fizzBuzzConversion() {
-        String output = "";
-        int oneHundred = Byte.MAX_VALUE - 27;
-        for (; number < oneHundred; number++)
-            output += checkForValue(number) + " ";
-        return output.substring(0, output.length() - 1);
+    String computeFizzBuzzUpToOneOuthundred() {
+        String result = "";
+        int maxFizzBuzz = Byte.MAX_VALUE - 27;
+
+        for (; number < maxFizzBuzz; number++)
+            result += calculateForValue(number) + " ";
+        String trimmedResult = result.substring(0, result.length() - 1);
+        return trimmedResult;
     }
 
-    private String checkForValue(int numberToCheck) {
-        FizzDivideThreeCheck++;
-        BuzzDivideFiveCheck--;
-        int binaryNumberThree = 0b11;
-        boolean divisibleByThree = FizzDivideThreeCheck == binaryNumberThree;
-        boolean divisibleByFive = BuzzDivideFiveCheck == 0;
-        String addValue = divisibleByThree || divisibleByFive ? "" : String.valueOf(numberToCheck + 1);
+    private String calculateForValue(int numberToCheck) {
+        FizzDivideThreeCounter++;
+        BuzzDivideFiveCounter--;
+        int three = 0b11;
+        boolean divisibleByThree = FizzDivideThreeCounter == three;
+        boolean divisibleByFive = BuzzDivideFiveCounter == 0;
+        String numericalValue = String.valueOf(numberToCheck + 1);
+        String addValue = divisibleByThree || divisibleByFive ? "" : numericalValue;
         if (divisibleByThree) addValue += fizz();
         if (divisibleByFive) addValue += buzz();
         return addValue;
     }
 
     private String buzz() {
-        BuzzDivideFiveCheck = new int[]{0, 0, 0, 0, 0}.length;
+        BuzzDivideFiveCounter = new int[]{0, 0, 0, 0, 0}.length;
         String buzz = new String(DatatypeConverter.parseHexBinary("42757a7a"), StandardCharsets.UTF_8);
         return buzz;
     }
 
     private String fizz() {
-        FizzDivideThreeCheck = 0;
+        FizzDivideThreeCounter = 0;
         String fizz = new String(DatatypeConverter.parseHexBinary("46697a7a"), StandardCharsets.UTF_8);
         return fizz;
     }
--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -2,71 +2,71 @@ package bowling;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-import static bowling.Bowling.*;
+
 
 public class BowlingTest {
 
     @Test
     public void allGutterBalls() {
         int [] rolls = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        Assertions.assertThat(score(rolls)).isEqualTo(0);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(0);
     }
 
     @Test
     public void noSparesOrStrikes() {
         int [] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-        Assertions.assertThat(score(rolls)).isEqualTo(20);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(20);
 
     }
 
     @Test
     public void oneSpare() {
         int [] rolls = {1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(29);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(29);
     }
 
 
     @Test
     public void gameWithTwoSpare() {
         int [] rolls = {1, 9, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(38);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(38);
     }
 
     @Test
     public void gameWithStrikeFirstRoll() {
         int [] rolls = {10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(30);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(30);
     }
 
 
     @Test
     public void gameWithStrikeFrame10() {
         int [] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(30);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(30);
     }
 
     @Test
     public void givenAllStrikes() {
         int [] rolls = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-        Assertions.assertThat(score(rolls)).isEqualTo(300);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(300);
     }
 
     @Test
     public void spareInFrameTen() {
         int [] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(29);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(29);
     }
 
     @Test
     public void fiveStrikesand5Spares() {
         int [] rolls = {10,10,10,10,10, 1, 9, 1,9, 1,9, 1,9, 1,9 , 1};
-        Assertions.assertThat(score(rolls)).isEqualTo(186);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(186);
     }
 
     @Test
     public void nineZeroEachFrame() {
         int [] rolls = {9,0, 9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0};
-        Assertions.assertThat(score(rolls)).isEqualTo(90);
+        Assertions.assertThat(Bowling.score(rolls)).isEqualTo(90);
     }
 }
