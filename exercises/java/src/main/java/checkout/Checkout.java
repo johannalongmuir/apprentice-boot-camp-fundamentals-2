296a409954f6ac066527107270e89f3dd89cbf5f
Johanna Longmuir <johannalongmuir@gmail.com>
2019-12-18 12:21:46 +0000

    All tests pass. Get line implimented

--- exercises/java/src/main/java/checkout/Checkout.java
+++ exercises/java/src/main/java/checkout/Checkout.java
@@ -5,29 +5,29 @@ class Checkout {
     public static final int DISCOUNT_VALUE_B = 15;
     public static final int NUMBER_OF_A_REQUIRED_FOR_DISOUNT = 5;
     public static final int NUMBER_OF_B_REQUIRED_FOR_DISCOUNT = 2;
-
+    public static final int NO_DISCOUNT_FOR_PRODUCT = 0;
     protected int totalCost;
-    private int numberOfA = 0;
-    private int numberOfB = 0;
+    protected int discountATracker = 0; // cannot be static
+    protected int discountBTracker = 0;
     private Receipt receipt = new Receipt();
 
-    void scan(String sku) {
-        if ("A".equals(sku)) {
+    void scan(String stockKeepingUnit) {
+        if ("A".equals(stockKeepingUnit)) {
             updateTotalCost(50);
-            ++numberOfA;
-            applyADiscounts(numberOfA, NUMBER_OF_A_REQUIRED_FOR_DISOUNT, DISCOUNT_VALUE_A);
-            receipt.scannedA(numberOfA);
-        } else if ("B".equals(sku)) {
+            ++discountATracker;
+            applyADiscounts(discountATracker, NUMBER_OF_A_REQUIRED_FOR_DISOUNT, DISCOUNT_VALUE_A);
+            receipt.getLine(stockKeepingUnit, 50, discountATracker);
+        } else if ("B".equals(stockKeepingUnit)) {
             updateTotalCost(30);
-            ++numberOfB;
-            applyADiscounts(numberOfB, NUMBER_OF_B_REQUIRED_FOR_DISCOUNT, DISCOUNT_VALUE_B);
-            receipt.scannedB(numberOfB);
-        } else if ("C".equals(sku)) {
+            ++discountBTracker;
+            applyADiscounts(discountBTracker, NUMBER_OF_B_REQUIRED_FOR_DISCOUNT, DISCOUNT_VALUE_B);
+            receipt.getLine(stockKeepingUnit, 30, discountBTracker);
+        } else if ("C".equals(stockKeepingUnit)) {
             updateTotalCost(20);
-            receipt.scannedC();
-        } else if ("D".equals(sku)) {
+            receipt.getLine(stockKeepingUnit, 20, NO_DISCOUNT_FOR_PRODUCT);
+        } else if ("D".equals(stockKeepingUnit)) {
             updateTotalCost(15);
-            receipt.scannedD();
+            receipt.getLine(stockKeepingUnit, 15, NO_DISCOUNT_FOR_PRODUCT);
         }
     }
 
--- exercises/java/src/main/java/checkout/Receipt.java
+++ exercises/java/src/main/java/checkout/Receipt.java
@@ -3,37 +3,42 @@ package checkout;
 class Receipt {
 
     private String text = "";
+    //private Checkout checkout = new Checkout();
 
     String text(int total) {
         return text + "Total: " + total;
     }
 
-    protected void scannedA(int numberOfA) {
-        text += "A: 50";
-        if (isDivisibleBy(numberOfA, Checkout.NUMBER_OF_A_REQUIRED_FOR_DISOUNT)) {
-            text += " - 30 (5 for 220)";
-        }
-        text += "\n";
+    void getLine(String stockKeepingUnit, int cost, int discount) {
+        text += stockKeepingUnit + ": " + cost;
+        discountLine(stockKeepingUnit, discount);
+        newLine();
     }
 
-
-
-    void scannedB(int numberOfB) {
-        text += "B: 30";
-        if (isDivisibleBy(numberOfB, Checkout.NUMBER_OF_B_REQUIRED_FOR_DISCOUNT)) {
+    private void discountLine(String stockKeepingUnit, int discount) {
+        if ("A".equals(stockKeepingUnit) && discount % Checkout.NUMBER_OF_A_REQUIRED_FOR_DISOUNT == 0) {
+            text += " - 30 (5 for 220)";
+        } else if ("B".equals(stockKeepingUnit) && discount % Checkout.NUMBER_OF_B_REQUIRED_FOR_DISCOUNT == 0) {
             text += " - 15 (2 for 45)";
         }
+    }
+
+    private void newLine() {
         text += "\n";
     }
 
-    void scannedC() {
-        text += "C: 20\n";
-    }
 
-    void scannedD() {
-        text += "D: 15\n";
-    }
+//    void scannedB(int numberOfB) {
+//        getLine("B: 30", 30);
+//        if (isDivisibleBy(numberOfB, Checkout.NUMBER_OF_B_REQUIRED_FOR_DISCOUNT)) {
+//            text += " - 15 (2 for 45)";
+//        }
+//        newLine();
+//    }
 
+
+//    if (isDivisibleBy(Checkout.numberOfA, Checkout.NUMBER_OF_A_REQUIRED_FOR_DISOUNT)) {
+//                text += " - 30 (5 for 220)";
 
 
     private boolean isDivisibleBy(int productCounter, int numberRequiredForDiscount) {
