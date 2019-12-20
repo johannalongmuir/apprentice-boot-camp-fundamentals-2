1cc034053f98b946c7852e283b7903cd4d8b9a50
Johanna Longmuir <johannalongmuir@gmail.com>
2019-12-20 15:35:16 +0000

    Refactor names

--- exercises/java/src/main/java/checkout/Checkout.java
+++ exercises/java/src/main/java/checkout/Checkout.java
@@ -5,29 +5,29 @@ class Checkout {
     public static final int DISCOUNT_VALUE_B = 15;
     public static final int NUMBER_OF_A_REQUIRED_FOR_DISOUNT = 5;
     public static final int NUMBER_OF_B_REQUIRED_FOR_DISCOUNT = 2;
-    public static final int NO_DISCOUNT_FOR_PRODUCT = 0;
+    public static final int PRODUCT_NO_DISCOUNT = 0;
     protected int totalCost;
-    protected int discountATracker = 0; // cannot be static
-    protected int discountBTracker = 0;
+    protected int numberOfA = 0; // cannot be static
+    protected int numberOfB = 0;
     private Receipt receipt = new Receipt();
 
     void scan(String stockKeepingUnit) {
         if ("A".equals(stockKeepingUnit)) {
             updateTotalCost(50);
-            ++discountATracker;
-            applyADiscounts(discountATracker, NUMBER_OF_A_REQUIRED_FOR_DISOUNT, DISCOUNT_VALUE_A);
-            receipt.getLine(stockKeepingUnit, 50, discountATracker);
+            ++numberOfA;
+            applyADiscounts(numberOfA, NUMBER_OF_A_REQUIRED_FOR_DISOUNT, DISCOUNT_VALUE_A);
+            receipt.getLine(stockKeepingUnit, 50, numberOfA);
         } else if ("B".equals(stockKeepingUnit)) {
             updateTotalCost(30);
-            ++discountBTracker;
-            applyADiscounts(discountBTracker, NUMBER_OF_B_REQUIRED_FOR_DISCOUNT, DISCOUNT_VALUE_B);
-            receipt.getLine(stockKeepingUnit, 30, discountBTracker);
+            ++numberOfB;
+            applyADiscounts(numberOfB, NUMBER_OF_B_REQUIRED_FOR_DISCOUNT, DISCOUNT_VALUE_B);
+            receipt.getLine(stockKeepingUnit, 30, numberOfB);
         } else if ("C".equals(stockKeepingUnit)) {
             updateTotalCost(20);
-            receipt.getLine(stockKeepingUnit, 20, NO_DISCOUNT_FOR_PRODUCT);
+            receipt.getLine(stockKeepingUnit, 20, PRODUCT_NO_DISCOUNT);
         } else if ("D".equals(stockKeepingUnit)) {
             updateTotalCost(15);
-            receipt.getLine(stockKeepingUnit, 15, NO_DISCOUNT_FOR_PRODUCT);
+            receipt.getLine(stockKeepingUnit, 15, PRODUCT_NO_DISCOUNT);
         }
     }
 
